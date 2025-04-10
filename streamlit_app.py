0e610e2da23d95391feceaa5cad97d8957c53c01
Timmy Weerwag <timmy.weerwag@navara.nl>
2025-04-10 15:50:17 +0200

    add nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -18,10 +19,17 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 ingredients_list = st.multiselect("Choose up to 5 ingredients:", my_dataframe, max_selections=5)
 
+def frootrequest(froot: str) -> str:
+    return requests.get(f"https://my.smoothiefroot.com/api/fruit/{froot}").json()
+
 if ingredients_list:
     ingredients_string = " ".join(ingredients_list)
     #my_insert_stmt = """ insert into smoothies.public.orders(name_on_order, ingredients)
     #        values ('""" + name_on_order + "','" + ingredients_string + """')"""
+
+    for fruit in ingredients_list:
+        st.subheader(f"{fruit} Nutrition Information")
+        sf_df = st.dataframe(frootrequest(fruit), use_container_width=True)
 
     time_to_insert = st.button("Submit Order")
     if time_to_insert:
